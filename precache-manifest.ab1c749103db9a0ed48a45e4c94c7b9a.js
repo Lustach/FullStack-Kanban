e0d9aec50cd645a88b5dab2546f53dfc490db1e1
lusta.vm <lusta.vlad2001@gmail.com>
2020-09-26 14:31:12 +0300

    deploy

---
+++ precache-manifest.ab1c749103db9a0ed48a45e4c94c7b9a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4391e48f5592c727ae9b",
+    "url": "/FullStack-Kanban/css/about.76874681.css"
+  },
+  {
+    "revision": "514ccd49a198ab1b3230",
+    "url": "/FullStack-Kanban/css/app.4efed78d.css"
+  },
+  {
+    "revision": "1cc7dc62e1db7c9c2beb",
+    "url": "/FullStack-Kanban/css/chunk-2d2bb58c.76874681.css"
+  },
+  {
+    "revision": "88241be8c1482bb71dda",
+    "url": "/FullStack-Kanban/css/chunk-b46c6446.90bcad6c.css"
+  },
+  {
+    "revision": "49c9bccd2c95ed2e982c",
+    "url": "/FullStack-Kanban/css/chunk-f88b53c8.85e964b6.css"
+  },
+  {
+    "revision": "5f78bdc586cd185315dd",
+    "url": "/FullStack-Kanban/css/chunk-vendors.433426e8.css"
+  },
+  {
+    "revision": "e332be546b6bcce18a0820b59def2fb8",
+    "url": "/FullStack-Kanban/index.html"
+  },
+  {
+    "revision": "4391e48f5592c727ae9b",
+    "url": "/FullStack-Kanban/js/about.6c9fab3d.js"
+  },
+  {
+    "revision": "514ccd49a198ab1b3230",
+    "url": "/FullStack-Kanban/js/app.43acd5d4.js"
+  },
+  {
+    "revision": "fdcbb74dd10d885e640c",
+    "url": "/FullStack-Kanban/js/chunk-2d0d3ff4.49a53a4a.js"
+  },
+  {
+    "revision": "8e360303028ec177a06a",
+    "url": "/FullStack-Kanban/js/chunk-2d238255.373f27c6.js"
+  },
+  {
+    "revision": "1cc7dc62e1db7c9c2beb",
+    "url": "/FullStack-Kanban/js/chunk-2d2bb58c.0119eb35.js"
+  },
+  {
+    "revision": "88241be8c1482bb71dda",
+    "url": "/FullStack-Kanban/js/chunk-b46c6446.21777ff7.js"
+  },
+  {
+    "revision": "49c9bccd2c95ed2e982c",
+    "url": "/FullStack-Kanban/js/chunk-f88b53c8.35aaaa64.js"
+  },
+  {
+    "revision": "5f78bdc586cd185315dd",
+    "url": "/FullStack-Kanban/js/chunk-vendors.23dfd567.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/FullStack-Kanban/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/FullStack-Kanban/robots.txt"
+  }
+]);
